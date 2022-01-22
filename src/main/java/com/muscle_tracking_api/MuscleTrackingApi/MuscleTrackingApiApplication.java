856ee88fe256f44ebccfba41dc8290e643ff541d
RyoIwahara <riwahara@mini.local>
2022-01-22 22:01:37 +0900

    first commit

---
+++ src/main/java/com/muscle_tracking_api/MuscleTrackingApi/MuscleTrackingApiApplication.java
@@ -0,0 +1,13 @@
+package com.muscle_tracking_api.MuscleTrackingApi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MuscleTrackingApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MuscleTrackingApiApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/muscle_tracking_api/MuscleTrackingApi/MuscleTrackingApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.muscle_tracking_api.MuscleTrackingApi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MuscleTrackingApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
